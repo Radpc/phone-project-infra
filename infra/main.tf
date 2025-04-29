e22d59ca63716a06b3ef7b1d87bb0799b8e1b7be
Matheus Marques <mgm3@icomp.ufam.edu.br>
2025-04-29 00:14:21 -0400

    feat: make port 3000 available

--- infra/main.tf
+++ infra/main.tf
@@ -54,6 +54,14 @@ resource "aws_security_group" "security-group" {
   }
 
   ingress {
+    description = "Backend API"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
