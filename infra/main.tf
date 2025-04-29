c3d8ad6c68fcb1b2b6aa2d43bfd00d8bba7fc19f
Matheus Marques <mgm3@icomp.ufam.edu.br>
2025-04-29 08:18:51 -0400

    fix: correct typo

--- infra/main.tf
+++ infra/main.tf
@@ -192,6 +192,6 @@ resource "aws_db_instance" "db_instance" {
   publicly_accessible    = true
   skip_final_snapshot    = true
   availability_zone      = "sa-east-1a"
-  vpc_security_group_ids = [aws_security_group.db-security-group]
+  vpc_security_group_ids = [aws_security_group.db-security-group.id]
   db_subnet_group_name   = aws_db_subnet_group.db-subnet-group.id
 }
